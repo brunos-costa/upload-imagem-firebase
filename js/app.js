bdcc37d05a76c9b161d00f2e426db9b4621ffaf4
Bruno Costa <analistabrunocosta@gmail.com>
2023-09-20 21:30:26 -0300

    Salvando urls das imagens em um vetor

--- js/app.js
+++ js/app.js
@@ -10,7 +10,9 @@ let btnCarregar = document.querySelector("#btnCarregar")
 
 let timestamp = new Date().getTime() // para poder ser utilizado como nome da imagem ao salvar no cloud storage.
 
-// Create a root reference
+let imagensUrl = []
+
+
 const storage = getStorage(app);
 const storageRef = ref(storage, `imagem/${timestamp}`);// função ref() recebe a instância de armazenamento storage e uma string que representa o caminho para o local desejado, seria o nome do arquivo lá no storage. Estou salvando dentro de uma pasta chamado 'imagem/' e em seguida
 
@@ -38,51 +40,62 @@ function store(){
     }
 }
 
+// FUNÇÃO PARA PODER PEGAR O CAMINHO DE CADA IMAGEM
 function exibirDados(){
     const listRef = ref(storage, "imagem");// buscando pela pasta e acessando todas as imagens dentro
-    // Get metadata properties
+
     listAll(listRef)
-    .then((res) => {
-      res.items.forEach((itemRef) => {
-        console.log(itemRef._location.path_)// pegando o caminho da imagem
-        downloadImagem(itemRef._location.path_)// passando o caminho da imagem, algo como 'imagem/1695246689148'
-      });
+    .then((resultado) => {           
+      resultado.items.forEach((itemRef) => {
+
+        //console.log(itemRef._location.path_)// pegando o caminho da imagem
+        gerarUrlImagem(itemRef._location.path_)// passando o caminho da imagem, algo como 'imagem/1695246689148'
+
+       
+      })
     }).catch((error) => {
       console.log("O seguinte erro ocorreu ao exibir tudo: ", error)
     });
+
+    console.log(imagensUrl)
 }
 
-async function downloadImagem(imagem){
-    const starsRef = ref(storage, imagem);//é preciso passar o caminho da imagem para poder gerar a url correta
+//FUNÇÃO PARA GERAR AS URLS DAS IMAGENS PARA PODER BAIXAR
+async function gerarUrlImagem(imagem){       
+    const resultado = ref(storage, imagem);//é preciso passar o caminho da imagem para poder gerar a url correta
 
-    try {
-        let resultado = await getDownloadURL(starsRef)
-        console.log(resultado)
-    } catch (error) {
+    getDownloadURL(resultado)
+    .then((url) => {
+        // Insert url into an <img> tag to "download"
+        console.log(url)
+        imagensUrl.push(url)// salvando todas as urls em um vetor
+        
+    })
+    .catch((error) => {
         // Uma lista completa de códigos de erro está disponível em
         // https://firebase.google.com/docs/storage/web/handle-errors
-        console.log(error)
         switch (error.code) {
-            case 'storage/object-not-found':
+        case 'storage/object-not-found':
             // File doesn't exist
             break;
-            case 'storage/unauthorized':
+        case 'storage/unauthorized':
             // User doesn't have permission to access the object
             break;
-            case 'storage/canceled':
+        case 'storage/canceled':
             // User canceled the upload
             break;
-    
-            // ...
-    
-            case 'storage/unknown':
+
+        // ...
+
+        case 'storage/unknown':
             // Unknown error occurred, inspect the server response
             break;
         }
-    }
+    });
 
    
 }
+
 
 btnEnviar.addEventListener("click", (evento)=>{
     evento.preventDefault()
@@ -95,8 +108,5 @@ btnCarregar.addEventListener("click",(evento)=>{
     evento.preventDefault()
 
     exibirDados()
-
-
-
 })
 
