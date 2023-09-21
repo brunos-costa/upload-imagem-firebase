f2e490f7a96eb29b00a817a80d28dca8331ff681
Bruno Costa <analistabrunocosta@gmail.com>
2023-09-21 14:19:22 -0300

    Salvando dados no Firestore

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import {app, db} from "./config-firebase.js"
-import { getFirestore, collection, addDoc, getDocs,query, orderBy, doc, deleteDoc, where, documentId, updateDoc  } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, listAll, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js";
+import { getStorage, ref, uploadBytes, getDownloadURL} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js"
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js"
 
 
 let imagem = document.querySelector("#arquivo")
@@ -10,92 +10,88 @@ let btnCarregar = document.querySelector("#btnCarregar")
 
 let timestamp = new Date().getTime() // para poder ser utilizado como nome da imagem ao salvar no cloud storage.
 
-let imagensUrl = []
+let dadosImagem = {
+    nome:"",
+    url: "",
+    timestamp: ""
+}
 
 
 const storage = getStorage(app);
 const storageRef = ref(storage, `imagem/${timestamp}`);// função ref() recebe a instância de armazenamento storage e uma string que representa o caminho para o local desejado, seria o nome do arquivo lá no storage. Estou salvando dentro de uma pasta chamado 'imagem/' e em seguida
 
 
-/* SALVANDO NO FIRESTORE */
 
-// REALIZANDO A INSERÇÃO NO BANCO
+// SALVANDO IMAGEM NO STORAGE E NO LOCALSTORAGE
 function store(){
-    try {
-        
+    try {        
         //console.log(imagem.files[0])
 
-        // Salvando imagem no Cloud Storage
+        dadosImagem.nome = imagem.files[0].name// salvando o nome da imagem
+        let data = new Date(timestamp)// convertendo o timestamp para data e hora
+        dadosImagem.timestamp = data.toLocaleString("pt-BR")// alterando a data e hora para o padrão BR
+
+      
+            // Fazendo o upload da imagem para o Storage
             uploadBytes(storageRef, imagem.files[0]).then((resultado) => {
                 console.log('Upload realizado', resultado);
-                alert("Upload realizado", resultado);
-            });
+
+                // Obtendo a URL de download da imagem
+                getDownloadURL(resultado.ref)
+                .then((url) => {
+
+                    console.log(url)
+                    dadosImagem.url = url
+
+                    localStorage.setItem("dados",JSON.stringify(dadosImagem))
+
+                    cadastrarImagem(dadosImagem)
+                })
+                .catch((error) => {
+                    // Uma lista completa de códigos de erro está disponível em
+                    // https://firebase.google.com/docs/storage/web/handle-errors
+                    switch (error.code) {
+                    case 'storage/object-not-found':
+                        // File doesn't exist
+                        break;
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break;
             
+                    // ...
             
-  
-       
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect the server response
+                        break;
+                    }
+                });            
+                
+            });            
+           
                
     } catch (error) {
         console.error("Erro ao criar o documento: ", error);
     }
 }
 
-// FUNÇÃO PARA PODER PEGAR O CAMINHO DE CADA IMAGEM
-function exibirDados(){
-    const listRef = ref(storage, "imagem");// buscando pela pasta e acessando todas as imagens dentro
-
-    listAll(listRef)
-    .then((resultado) => {           
-      resultado.items.forEach((itemRef) => {
-
-        //console.log(itemRef._location.path_)// pegando o caminho da imagem
-        gerarUrlImagem(itemRef._location.path_)// passando o caminho da imagem, algo como 'imagem/1695246689148'
-
-       
-      })
-    }).catch((error) => {
-      console.log("O seguinte erro ocorreu ao exibir tudo: ", error)
-    });
-
-    console.log(imagensUrl)
-}
-
-//FUNÇÃO PARA GERAR AS URLS DAS IMAGENS PARA PODER BAIXAR
-async function gerarUrlImagem(imagem){       
-    const resultado = ref(storage, imagem);//é preciso passar o caminho da imagem para poder gerar a url correta
-
-    getDownloadURL(resultado)
-    .then((url) => {
-        // Insert url into an <img> tag to "download"
-        console.log(url)
-        imagensUrl.push(url)// salvando todas as urls em um vetor
+async function cadastrarImagem(dados){
+    try {
+        const docRef = await addDoc(collection(db, "imagens"), dados);
+        //console.log("Document criado com ID: ", docRef.id);
+        alert("Imagem cadastrada com sucesso");
         
-    })
-    .catch((error) => {
-        // Uma lista completa de códigos de erro está disponível em
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-        case 'storage/object-not-found':
-            // File doesn't exist
-            break;
-        case 'storage/unauthorized':
-            // User doesn't have permission to access the object
-            break;
-        case 'storage/canceled':
-            // User canceled the upload
-            break;
+        //resetar() 
 
-        // ...
 
-        case 'storage/unknown':
-            // Unknown error occurred, inspect the server response
-            break;
-        }
-    });
-
-   
+    } catch (e) {
+        console.error("Erro ao criar o documento: ", e);
+    }
+    
+    //getDados() // chamando função para atualizar lista de dados ao criar novo contato
 }
-
 
 btnEnviar.addEventListener("click", (evento)=>{
     evento.preventDefault()
